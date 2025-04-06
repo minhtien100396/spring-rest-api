87386b2093193852e09ce359baebee0c2ca2df51
Bui Minh Tien <minhtien100396@gmail.com>
2025-04-06 18:42:00 +0700

    update

--- api-spring/src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ api-spring/src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -58,8 +59,10 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(
                         authz -> authz
                                 // khong can xac thuc van truy cap duoc
-                                .requestMatchers(whileList)
-                                .permitAll()
+                                .requestMatchers(whileList).permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/companies").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/jobs").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/skills").permitAll()
                                 // phai xac thuc moi duoc truy cap
                                 .anyRequest().authenticated())
                 // dung de xac thuc nguoi dung thong qua ham jwtDecoder()
